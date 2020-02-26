a1470e85a271fa04b136dff7551f7fef129ed541
pshiyani <61038001+pshiyani@users.noreply.github.com>
2020-02-26 00:43:53 -0500

    Update HW_04A_pshiyani.py

--- HW_04A_pshiyani.py
+++ HW_04A_pshiyani.py
@@ -3,7 +3,7 @@ SSW_567
 Author: Priyankaben Shiyani
 Project Description: Develop with the Perspective of the Tester in mind
 """
-
+import json
 import requests
 
 def get_id_user():
@@ -14,29 +14,32 @@ def get_id_user():
     except ValueError:
         return " That was not valid input. Please try again."
 
-def get_data_user(get):
-    ''' fetches user list of names of repositories of a GitHub account and 
-        number of commits for a repository
-     '''
-    try:
-        repost_url = f"https://api.github.com/users/{get}/repos"
-        github_data_user = requests.get(repost_url).json() # fetches and converts to JSON
-        di = dict()
-        for item in github_data_user:
-            commit_url = f"https://api.github.com/repos/{get}/{item}/commits"
-            req = requests.get(commit_url).json() # fetches and converts to JSON
-            di[item['name']] = len(req)
-        return di
-        
-    except ValueError:
-        print('Bad data.')
+def get_user_data(user_id):
+    r = requests.get(f'https://api.github.com/users/{user_id}/repos')
+    if '"message":"Not Found"' in r.text:
+        return "The user ID does not exist"
+    else:
+        repository = {}
+        res_json = json.loads(r.text)
+        for repo in res_json:
+            repository[repo['name']] = 0
 
+        if len(repository.keys()) == 0:
+            return "This user has no repository"
+        else:
+            """Returning a list of names of repositories of a GitHub account and number of commits for a repository"""
+            for item in repository.keys():
+                repository[item] = len(json.loads((requests.get(f'https://api.github.com/repos/'
+                                                                f'{user_id}/{item}/commits')).text))
+           
+            for repo, commit_count in repository.items():
+                print(f"Repo: {repo} Number of commits: {commit_count}")
+        return repository
 def main():
     """main function"""
     try:
-        get_data_user(get_id_user())
+        get_user_data(get_id_user())
     except ValueError:
         print('Bad data. Please try again.')
-
 if __name__ == '__main__':
     main()
